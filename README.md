602b2650a1bfe7ea9e6211b75c917fe7bf94d633
Aiboo13 <minasssahiddin@gmail.com>
2026-02-27 22:09:53 +0700

    praktikum pertama

---
+++ README.md
@@ -0,0 +1,15 @@
+|  Nama | Moh. Minas Sahiddin |
+|:-----:|---------------------|
+|  Nrp  | 3124521045          |
+| Kelas | 2 TI - B            |
+---
+
+# Praktikum 1
+---
+tugas:
+```
+menambahkan fungsi button decrement
+```
+
+result ada di [tugas](/hasil.png)
+---
---
+++ lib/main.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: .fromSeed(
+          seedColor: const Color.fromARGB(255, 0, 255, 255),
+        ),
+      ),
+      home: const MyHomePage(title: 'tugas perdana'),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  void _decrementCounter() {
+    setState(() {
+      _counter--;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: .center,
+          children: [
+            const Column(
+              children: [
+                Text("Nama: MOH. MINAS SAHIDDIN"),
+                Text(" NIM: 123200035"),
+                Text("Jumlah Tekan: "),
+              ],
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          FloatingActionButton(
+            onPressed: _incrementCounter,
+            tooltip: 'Increment',
+            child: const Icon(Icons.add),
+          ),
+          const SizedBox(width: 16), // adding some space between the buttons
+          FloatingActionButton(
+            onPressed: _decrementCounter,
+            tooltip: 'Decrement',
+            child: const Icon(Icons.remove),
+          ),
+        ],
+      ),
+      // adding button for decrementing the counter
+    );
+  }
+}
